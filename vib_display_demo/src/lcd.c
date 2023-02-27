e5fe19c6a1330d835c67472aa2818174733481ed
Andrew Hedin <andrew.hedin@lairdconnect.com>
2023-02-27 14:06:07 -0600

    compiling
    Signed-off-by: Andrew Hedin <andrew.hedin@lairdconnect.com>

--- vib_display_demo/src/lcd.c
+++ vib_display_demo/src/lcd.c
@@ -64,8 +64,8 @@ static uint8_t chart_readings = 0;
 /******************************************************************************/
 /* Local Function Prototypes                                                  */
 /******************************************************************************/
-static void checkbox_event_handler(lv_obj_t *obj, lv_event_t event);
-static void button_event_handler(lv_obj_t *obj, lv_event_t event);
+static void checkbox_event_handler(lv_event_t *event);
+static void button_event_handler(lv_event_t *event);
 static void lcd_display_update_handler(struct k_work *work);
 static void lcd_display_update_timer_handler(struct k_timer *dummy);
 
@@ -80,41 +80,35 @@ K_MSGQ_DEFINE(lcd_event_queue, sizeof(struct lcd_event_s),
 /******************************************************************************/
 /* Local Function Definitions                                                 */
 /******************************************************************************/
-static void checkbox_event_handler(lv_obj_t *obj, lv_event_t event)
+static void checkbox_event_handler(lv_event_t *event)
 {
 	/* Only process events where a checkbox has been ticked or unticked */
-	if (event == LV_EVENT_VALUE_CHANGED) {
+	if (event->code == LV_EVENT_VALUE_CHANGED) {
 		lv_chart_series_t *series = NULL;
 		int16_t *chart_data = NULL;
 
-		if (obj == ui_check_x) {
+		if (event->target == ui_check_x) {
 			series = chart_series_x;
 			chart_data = chart_data_buffer_x;
-		} else if (obj == ui_check_y) {
+		} else if (event->target == ui_check_y) {
 			series = chart_series_y;
 			chart_data = chart_data_buffer_y;
-		} else if (obj == ui_check_z) {
+		} else if (event->target == ui_check_z) {
 			series = chart_series_z;
 			chart_data = chart_data_buffer_z;
 		}
 
 		if (series != NULL) {
-			if (lv_checkbox_is_checked(obj)) {
-				/* Checkbox was ticked, add the data to the
-				 * graph
-				 */
+			if (lv_obj_get_state(event->target) & LV_STATE_CHECKED) {
 				uint8_t i = CONFIG_APP_LCD_DATA_POINTS -
 					    chart_readings;
 				while (i < CONFIG_APP_LCD_DATA_POINTS) {
-					lv_chart_set_next(ui_chart, series,
-							  chart_data[i]);
+					lv_chart_set_next_value(ui_chart, series, chart_data[i]);
 					++i;
 				}
+				lv_chart_hide_series(ui_chart, series, false);
 			} else {
-				/* Checkbox was unticked, clear the series
-				 * data
-				 */
-				lv_chart_clear_series(ui_chart, series);
+				lv_chart_hide_series(ui_chart, series, true);
 			}
 
 			lv_chart_refresh(ui_chart);
@@ -122,11 +116,11 @@ static void checkbox_event_handler(lv_obj_t *obj, lv_event_t event)
 	}
 }
 
-static void button_event_handler(lv_obj_t *obj, lv_event_t event)
+static void button_event_handler(lv_event_t *event)
 {
 	/* Only process events where a button has been pressed */
-	if (event == LV_EVENT_CLICKED) {
-		if (obj == ui_button_startstop) {
+	if (event->code == LV_EVENT_CLICKED) {
+		if (event->target == ui_button_startstop) {
 			/* Start/stop button has been pressed, send a message
 			 * to any receivers to inform them of the button press
 			 */
@@ -156,21 +150,16 @@ static void button_event_handler(lv_obj_t *obj, lv_event_t event)
 				k_msgq_purge(&lcd_event_queue);
 				LOG_DBG("lcd_event_queue was cleared");
 			}
-		} else if (obj == ui_button_clear) {
-			/* Clear all the buffered data and remove the data from the
-			 * graph
-			 */
-			memset(chart_data_buffer_x, 0,
-			       CONFIG_APP_LCD_DATA_POINTS);
-			memset(chart_data_buffer_y, 0,
-			       CONFIG_APP_LCD_DATA_POINTS);
-			memset(chart_data_buffer_z, 0,
-			       CONFIG_APP_LCD_DATA_POINTS);
+		} else if (event->target == ui_button_clear) {
+			/* Clear all the buffered data. */
+			memset(chart_data_buffer_x, 0, CONFIG_APP_LCD_DATA_POINTS);
+			memset(chart_data_buffer_y, 0, CONFIG_APP_LCD_DATA_POINTS);
+			memset(chart_data_buffer_z, 0, CONFIG_APP_LCD_DATA_POINTS);
 			chart_readings = 0;
 
-			lv_chart_clear_series(ui_chart, chart_series_x);
-			lv_chart_clear_series(ui_chart, chart_series_y);
-			lv_chart_clear_series(ui_chart, chart_series_z);
+			lv_chart_hide_series(ui_chart, chart_series_x, true);
+			lv_chart_hide_series(ui_chart, chart_series_y, true);
+			lv_chart_hide_series(ui_chart, chart_series_z, true);
 
 			lv_chart_refresh(ui_chart);
 		}
@@ -218,129 +207,88 @@ void SetupLCD(void)
 	 * the graph is a container with a start/stop button and a clear button.
 	 * All objects are aligned to their centres
 	 */
-	ui_container_main = lv_cont_create(lv_scr_act(), NULL);
-	lv_obj_set_auto_realign(ui_container_main, true);
-	lv_cont_set_fit(ui_container_main, LV_FIT_TIGHT);
-	lv_cont_set_layout(ui_container_main, LV_LAYOUT_COLUMN_MID);
+	ui_container_main = lv_obj_create(lv_scr_act());
+	lv_obj_set_layout(ui_container_main, LV_ALIGN_OUT_TOP_MID);
 
-	ui_container_graph = lv_cont_create(ui_container_main, NULL);
-	lv_obj_set_auto_realign(ui_container_graph, true);
-	lv_cont_set_fit(ui_container_graph, LV_FIT_TIGHT);
-	lv_cont_set_layout(ui_container_graph, LV_LAYOUT_ROW_MID);
+	ui_container_graph = lv_obj_create(ui_container_main);
+	lv_obj_set_layout(ui_container_graph, LV_ALIGN_OUT_TOP_MID);
 
-	lv_obj_set_style_local_pad_top(ui_container_graph, LV_OBJ_PART_MAIN,
-				       LV_STATE_DEFAULT, CONTAINER_PADDING);
-	lv_obj_set_style_local_pad_bottom(ui_container_graph, LV_OBJ_PART_MAIN,
-					  LV_STATE_DEFAULT, CONTAINER_PADDING);
-	lv_obj_set_style_local_pad_left(ui_container_graph, LV_OBJ_PART_MAIN,
-					LV_STATE_DEFAULT, CONTAINER_PADDING);
-	lv_obj_set_style_local_pad_right(ui_container_graph, LV_OBJ_PART_MAIN,
-					 LV_STATE_DEFAULT, CONTAINER_PADDING);
+	lv_obj_set_style_pad_all(ui_container_graph, CONTAINER_PADDING, 0);
+	
+	ui_chart = lv_chart_create(ui_container_graph);
 
-	ui_chart = lv_chart_create(ui_container_graph, NULL);
+	ui_container_selections = lv_obj_create(ui_container_graph);
+	lv_obj_set_layout(ui_container_selections, LV_ALIGN_OUT_TOP_LEFT);
 
-	ui_container_selections = lv_cont_create(ui_container_graph, NULL);
-	lv_obj_set_auto_realign(ui_container_selections, true);
-	lv_cont_set_fit(ui_container_selections, LV_FIT_TIGHT);
-	lv_cont_set_layout(ui_container_selections, LV_LAYOUT_COLUMN_LEFT);
+	lv_obj_set_style_pad_all(ui_container_selections, CONTAINER_PADDING, 0);
 
-	lv_obj_set_style_local_pad_top(ui_container_selections,
-				       LV_OBJ_PART_MAIN, LV_STATE_DEFAULT,
-				       CONTAINER_PADDING);
-	lv_obj_set_style_local_pad_bottom(ui_container_selections,
-					  LV_OBJ_PART_MAIN, LV_STATE_DEFAULT,
-					  CONTAINER_PADDING);
-	lv_obj_set_style_local_pad_left(ui_container_selections,
-					LV_OBJ_PART_MAIN, LV_STATE_DEFAULT,
-					CONTAINER_PADDING);
-	lv_obj_set_style_local_pad_right(ui_container_selections,
-					 LV_OBJ_PART_MAIN, LV_STATE_DEFAULT,
-					 CONTAINER_PADDING);
-
-	lv_chart_set_y_range(ui_chart, LV_CHART_AXIS_PRIMARY_Y,
+	lv_chart_set_range(ui_chart, LV_CHART_AXIS_PRIMARY_Y,
 			     CHART_Y_PRIMARY_MIN, CHART_Y_PRIMARY_MAX);
 
 	lv_obj_set_size(ui_chart, CHART_WIDTH, CHART_HEIGHT);
-	lv_obj_align(ui_chart, NULL, LV_ALIGN_IN_TOP_MID, 0, 0);
+	lv_obj_align(ui_chart, LV_ALIGN_OUT_TOP_MID, 0, 0);
 	lv_chart_set_type(ui_chart, LV_CHART_TYPE_LINE);
 
 	lv_chart_set_point_count(ui_chart, CONFIG_APP_LCD_DATA_POINTS);
 
+#if 0
 	lv_chart_set_x_tick_texts(ui_chart, "old\nnew", 1,
 				  LV_CHART_AXIS_DRAW_LAST_TICK);
 	lv_chart_set_y_tick_texts(ui_chart, "4\n2\n0\n-2\n-4", 1,
 				  LV_CHART_AXIS_DRAW_LAST_TICK);
+#endif
 
-	lv_obj_set_style_local_pad_top(ui_chart, LV_OBJ_PART_MAIN,
-				       LV_STATE_DEFAULT, CHART_PADDING_TOP);
-	lv_obj_set_style_local_pad_bottom(ui_chart, LV_OBJ_PART_MAIN,
-					  LV_STATE_DEFAULT,
-					  CHART_PADDING_BOTTOM);
-	lv_obj_set_style_local_pad_left(ui_chart, LV_OBJ_PART_MAIN,
-					LV_STATE_DEFAULT, CHART_PADDING_LEFT);
-	lv_obj_set_style_local_pad_right(ui_chart, LV_OBJ_PART_MAIN,
-					 LV_STATE_DEFAULT, CHART_PADDING_RIGHT);
+	lv_obj_set_style_pad_top(ui_chart, CHART_PADDING_TOP, 0);
+	lv_obj_set_style_pad_bottom(ui_chart, CHART_PADDING_BOTTOM, 0);
+	lv_obj_set_style_pad_left(ui_chart, CHART_PADDING_LEFT, 0);
+	lv_obj_set_style_pad_right(ui_chart, CHART_PADDING_RIGHT, 0);
 
-	chart_series_x = lv_chart_add_series(ui_chart, LV_COLOR_RED);
-	chart_series_y = lv_chart_add_series(ui_chart, LV_COLOR_YELLOW);
-	chart_series_z = lv_chart_add_series(ui_chart, LV_COLOR_GREEN);
+	chart_series_x = lv_chart_add_series(ui_chart, lv_palette_main(LV_PALETTE_RED),
+					     LV_CHART_AXIS_PRIMARY_X);
+	chart_series_y = lv_chart_add_series(ui_chart, lv_palette_main(LV_PALETTE_YELLOW),
+					     LV_CHART_AXIS_PRIMARY_X);
+	chart_series_z = lv_chart_add_series(ui_chart, lv_palette_main(LV_PALETTE_GREEN),
+					     LV_CHART_AXIS_PRIMARY_X);
 
-	ui_check_x = lv_checkbox_create(ui_container_selections, NULL);
-	lv_checkbox_set_checked(ui_check_x, true);
+	ui_check_x = lv_checkbox_create(ui_container_selections);
+	lv_obj_add_state(ui_check_x, LV_STATE_CHECKED);
 	lv_checkbox_set_text(ui_check_x, "X");
-	lv_obj_align(ui_check_x, NULL, LV_ALIGN_CENTER, 0, 0);
-	lv_obj_set_event_cb(ui_check_x, checkbox_event_handler);
-	lv_obj_set_style_local_bg_color(ui_check_x, LV_CHECKBOX_PART_BULLET,
-					LV_STATE_CHECKED, LV_COLOR_RED);
-	lv_obj_set_style_local_border_color(ui_check_x, LV_CHECKBOX_PART_BULLET,
-					    LV_STATE_DEFAULT, LV_COLOR_RED);
+	lv_obj_align(ui_check_x, LV_ALIGN_CENTER, 0, 0);
+	lv_obj_add_event_cb(ui_check_x, checkbox_event_handler, LV_EVENT_PRESSED, NULL);
+	lv_obj_set_style_bg_color(ui_check_x, lv_palette_main(LV_PALETTE_RED), 0);
+	lv_obj_set_style_border_color(ui_check_x, lv_palette_main(LV_PALETTE_RED), 0);
 
-	ui_check_y = lv_checkbox_create(ui_container_selections, NULL);
-	lv_checkbox_set_checked(ui_check_y, true);
+	ui_check_y = lv_checkbox_create(ui_container_selections);
+	lv_obj_add_state(ui_check_y, LV_STATE_CHECKED);
 	lv_checkbox_set_text(ui_check_y, "Y");
-	lv_obj_align(ui_check_y, NULL, LV_ALIGN_CENTER, 0, 0);
-	lv_obj_set_event_cb(ui_check_y, checkbox_event_handler);
-	lv_obj_set_style_local_bg_color(ui_check_y, LV_CHECKBOX_PART_BULLET,
-					LV_STATE_CHECKED, LV_COLOR_YELLOW);
-	lv_obj_set_style_local_border_color(ui_check_y, LV_CHECKBOX_PART_BULLET,
-					    LV_STATE_DEFAULT, LV_COLOR_YELLOW);
+	lv_obj_align(ui_check_y, LV_ALIGN_CENTER, 0, 0);
+	lv_obj_add_event_cb(ui_check_y, checkbox_event_handler, LV_EVENT_PRESSED, NULL);
+	lv_obj_set_style_bg_color(ui_check_y, lv_palette_main(LV_PALETTE_YELLOW), 0);
+	lv_obj_set_style_border_color(ui_check_y, lv_palette_main(LV_PALETTE_YELLOW), 0);
 
-	ui_check_z = lv_checkbox_create(ui_container_selections, NULL);
-	lv_checkbox_set_checked(ui_check_z, true);
+	ui_check_z = lv_checkbox_create(ui_container_selections);
+	lv_obj_add_state(ui_check_z, LV_STATE_CHECKED);
 	lv_checkbox_set_text(ui_check_z, "Z");
-	lv_obj_align(ui_check_z, NULL, LV_ALIGN_CENTER, 0, 0);
-	lv_obj_set_event_cb(ui_check_z, checkbox_event_handler);
-	lv_obj_set_style_local_bg_color(ui_check_z, LV_CHECKBOX_PART_BULLET,
-					LV_STATE_CHECKED, LV_COLOR_GREEN);
-	lv_obj_set_style_local_border_color(ui_check_z, LV_CHECKBOX_PART_BULLET,
-					    LV_STATE_DEFAULT, LV_COLOR_GREEN);
+	lv_obj_align(ui_check_z, LV_ALIGN_CENTER, 0, 0);
+	lv_obj_add_event_cb(ui_check_z, checkbox_event_handler, LV_EVENT_PRESSED, NULL);
+	lv_obj_set_style_bg_color(ui_check_z, lv_palette_main(LV_PALETTE_GREEN), 0);
+	lv_obj_set_style_border_color(ui_check_z, lv_palette_main(LV_PALETTE_GREEN), 0);
 
-	ui_container_buttons = lv_cont_create(ui_container_main, NULL);
-	lv_obj_set_auto_realign(ui_container_buttons, true);
-	lv_cont_set_fit(ui_container_buttons, LV_FIT_TIGHT);
-	lv_cont_set_layout(ui_container_buttons, LV_LAYOUT_ROW_MID);
+	ui_container_buttons = lv_obj_create(ui_container_main);
+	lv_obj_set_layout(ui_container_buttons, LV_ALIGN_OUT_TOP_MID);
 
-	lv_obj_set_style_local_pad_top(ui_container_graph, LV_OBJ_PART_MAIN,
-				       LV_STATE_DEFAULT, CONTAINER_PADDING);
-	lv_obj_set_style_local_pad_bottom(ui_container_graph, LV_OBJ_PART_MAIN,
-					  LV_STATE_DEFAULT, CONTAINER_PADDING);
-	lv_obj_set_style_local_pad_left(ui_container_graph, LV_OBJ_PART_MAIN,
-					LV_STATE_DEFAULT, CONTAINER_PADDING);
-	lv_obj_set_style_local_pad_right(ui_container_graph, LV_OBJ_PART_MAIN,
-					 LV_STATE_DEFAULT, CONTAINER_PADDING);
+	lv_obj_set_style_pad_all(ui_container_graph, CONTAINER_PADDING, 0);
 
-	ui_button_startstop = lv_btn_create(ui_container_buttons, NULL);
-	lv_obj_align(ui_button_startstop, NULL, LV_ALIGN_CENTER, 0, 0);
-	lv_btn_set_fit(ui_button_startstop, LV_FIT_TIGHT);
-	lv_obj_set_event_cb(ui_button_startstop, button_event_handler);
-	ui_text_startstop = lv_label_create(ui_button_startstop, NULL);
+	ui_button_startstop = lv_btn_create(ui_container_buttons);
+	lv_obj_align(ui_button_startstop, LV_ALIGN_CENTER, 0, 0);
+	lv_obj_add_event_cb(ui_button_startstop, button_event_handler, LV_EVENT_PRESSED, NULL);
+	ui_text_startstop = lv_label_create(ui_button_startstop);
 	lv_label_set_text(ui_text_startstop, STARTSTOP_BUTTON_START_TEXT);
 
-	ui_button_clear = lv_btn_create(ui_container_buttons, NULL);
-	lv_obj_align(ui_button_clear, NULL, LV_ALIGN_CENTER, 0, 0);
-	lv_btn_set_fit(ui_button_clear, LV_FIT_TIGHT);
-	lv_obj_set_event_cb(ui_button_clear, button_event_handler);
-	ui_text_clear = lv_label_create(ui_button_clear, NULL);
+	ui_button_clear = lv_btn_create(ui_container_buttons);
+	lv_obj_align(ui_button_clear, LV_ALIGN_CENTER, 0, 0);
+	lv_obj_add_event_cb(ui_button_clear, button_event_handler, LV_EVENT_PRESSED, NULL);
+	ui_text_clear = lv_label_create(ui_button_clear);
 	lv_label_set_text(ui_text_clear, "Clear");
 
 	display_blanking_off(display_dev);
@@ -399,22 +347,19 @@ void UpdateLCDGraph(int16_t x, int16_t y, int16_t z)
 	/* Only add the data to the graph if the respective checkbox is
 	 * ticked
 	 */
-	if (lv_checkbox_is_checked(ui_check_x)) {
-		lv_chart_set_next(
-			ui_chart, chart_series_x,
-			chart_data_buffer_x[CONFIG_APP_LCD_DATA_POINTS - 1]);
+	if (lv_obj_get_state(ui_check_x) & LV_STATE_CHECKED) {
+		lv_chart_set_next_value(ui_chart, chart_series_x,
+					chart_data_buffer_x[CONFIG_APP_LCD_DATA_POINTS - 1]);
 	}
 
-	if (lv_checkbox_is_checked(ui_check_y)) {
-		lv_chart_set_next(
-			ui_chart, chart_series_y,
-			chart_data_buffer_y[CONFIG_APP_LCD_DATA_POINTS - 1]);
+	if (lv_obj_get_state(ui_check_y) & LV_STATE_CHECKED) {
+		lv_chart_set_next_value(ui_chart, chart_series_y,
+					chart_data_buffer_y[CONFIG_APP_LCD_DATA_POINTS - 1]);
 	}
 
-	if (lv_checkbox_is_checked(ui_check_z)) {
-		lv_chart_set_next(
-			ui_chart, chart_series_z,
-			chart_data_buffer_z[CONFIG_APP_LCD_DATA_POINTS - 1]);
+	if (lv_obj_get_state(ui_check_z) & LV_STATE_CHECKED) {
+		lv_chart_set_next_value(ui_chart, chart_series_z,
+					chart_data_buffer_z[CONFIG_APP_LCD_DATA_POINTS - 1]);
 	}
 }
 
